60f610a5f5682f4fe29d69c9af7e36ce52d58b05
Kerciu <Kerciuuu@gmail.com>
2024-08-07 11:17:47 +0200

    added current date time fetching functions

--- src/main/java/Backend/RetrieveAPIData.java
+++ src/main/java/Backend/RetrieveAPIData.java
@@ -8,12 +8,24 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class RetrieveAPIData {
     public static JSONObject getWeatherData(String locationName)
     {
         JSONArray jsonArray = getGeographicCoordinates(locationName);
+
+        // get the first search result, in case there is for example
+        // "berlin" village in remote russian siberia
+        JSONObject location = (JSONObject) jsonArray.get(0);
+        double latitude = (double) location.get("latitude");
+        double longitude = (double) location.get("longitude");
+
+        String urlString = buildStringURLwithCoordinates(latitude, longitude);
+        JSONObject hourlyData = (JSONObject) callAPI(urlString).get("hourly");
+        JSONArray timeData = (JSONArray) hourlyData.get("time");
 
         return null;
     }
@@ -22,6 +34,35 @@ public class RetrieveAPIData {
     {
         locationName = locationName.replaceAll(" ", "+");
         return (JSONArray) callAPI(buildLocationURL(locationName)).get("results");
+    }
+
+    private static int findIndexOfCurrentTime(JSONArray timeData)
+    {
+        String currentTime = getCurrentTime();
+    }
+
+    private static String getCurrentTime()
+    {
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        // format date to be something like ""2024-08-10T05:00"
+        return localDateTime.format(createDateTimeFormatter());
+    }
+
+    private static DateTimeFormatter createDateTimeFormatter()
+    {
+        String formatPattern = "yyyy-MM-dd'T'HH':00'";
+        return DateTimeFormatter.ofPattern(formatPattern);
+    }
+
+    private static String buildStringURLwithCoordinates(double latitude, double longitude)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("https://api.open-meteo.com/v1/forecast?latitude=");
+        stringBuilder.append(Double.toString(latitude));
+        stringBuilder.append("&" + Double.toString(longitude));
+        stringBuilder.append("&hourly=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m");
+        return stringBuilder.toString();
     }
 
     private static String buildLocationURL(String locationName)

67dc82e0feb21dd750e52fbb1c702e3c1c1b90fe
Kerciu <Kerciuuu@gmail.com>
2024-08-08 21:36:38 +0200

    added tests

--- src/test/java/BackendTests/GeocodingServiceTest.java
+++ src/test/java/BackendTests/GeocodingServiceTest.java
@@ -1,4 +1,25 @@
 package test.java.BackendTests;
 
+import main.java.backend.api.APIUtils;
+import main.java.backend.service.GeocodingService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class GeocodingServiceTest {
+    @Test
+    void testGetGeographicCoordinates_Success() {
+        String locationName = "Berlin";
+        String url = "https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json";
+    }
+
+    @Test
+    void testGetGeographicCoordinates_NoResults() {
+        String locationName = "UnknownPlace";
+        String url = "https://geocoding-api.open-meteo.com/v1/search?name=UnknownPlace&count=10&language=en&format=json";
+    }
 }
--- src/test/java/BackendTests/WeatherServiceTest.java
+++ src/test/java/BackendTests/WeatherServiceTest.java
@@ -1,4 +1,24 @@
 package test.java.BackendTests;
 
+import main.java.backend.model.WeatherData;
+import main.java.backend.service.WeatherService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class WeatherServiceTest {
+    @Test
+    public void testGetWeatherData() {
+        double latitude = 52.2297;
+        double longitude = 21.0122;
+
+        WeatherData weatherData = WeatherService.getWeatherData(latitude, longitude);
+
+        assertNotNull(weatherData);
+        assertNotNull(weatherData.toJSON().get("weather_condition"));
+        assertTrue(weatherData.toJSON().get("temperature") instanceof Double);
+        assertTrue(weatherData.toJSON().get("humidity") instanceof Double);
+        assertTrue(weatherData.toJSON().get("wind_speed") instanceof Double);
+    }
 }

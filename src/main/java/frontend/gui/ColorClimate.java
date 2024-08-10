a5106938602830e04913ace9cfe328f881fbbb20
Kerciu <Kerciuuu@gmail.com>
2024-08-10 10:07:50 +0200

    updated code of switch statement

--- src/main/java/frontend/gui/ColorClimate.java
+++ src/main/java/frontend/gui/ColorClimate.java
@@ -18,49 +18,17 @@ public class ColorClimate {
 
     public Color[] getColorPalette() {
         return switch (condition) {
-            case SUNNY -> {
-                if (temperature > 35) {
-                    yield ColorConstants.SUNNY_ABOVE_35;
-                } else if (temperature > 25) {
-                    yield ColorConstants.SUNNY_ABOVE_25_AND_BELOW_35;
-                } else {
-                    yield ColorConstants.SUNNY_BELOW_25;
-                }
-            }
-            case SUNNY_CLOUDY -> {
-                if (temperature > 30) {
-                    yield ColorConstants.SUNNY_CLOUDY_ABOVE_30;
-                } else {
-                    yield ColorConstants.SUNNY_CLOUDY_BELOW_30;
-                }
-            }
-            case CLOUDY -> {
-                if (temperature > 25) {
-                    yield ColorConstants.CLOUDY_ABOVE_25;
-                } else {
-                    yield ColorConstants.CLOUDY_BELOW_25;
-                }
-            }
+            case SUNNY -> getSunnyPalette();
+            case SUNNY_CLOUDY -> getSunnyCloudyPalette();
+            case CLOUDY -> getCloudyPalette();
             case FOG -> ColorConstants.FOGGY;
             case DRIZZLE -> ColorConstants.DRIZZLY;
-            case RAIN -> {
-                if (temperature > 25) {
-                    yield ColorConstants.RAINY_ABOVE_25;
-                } else {
-                    yield ColorConstants.RAINY_BELOW_25;
-                }
-            }
+            case RAIN -> getRainPalette();
             case SNOWY -> ColorConstants.SNOWY;
             case DOWNPOUR -> ColorConstants.DOWNPOUR;
             case SLEET -> ColorConstants.SLEET;
             case SNOWSTORM -> ColorConstants.SNOWSTORM;
-            case THUNDERSTORM -> {
-                if (temperature > 25) {
-                    yield ColorConstants.THUNDERSTORM_ABOVE_25;
-                } else {
-                    yield ColorConstants.THUNDERSTORM_BELOW_25;
-                }
-            }
+            case THUNDERSTORM -> getThunderstormPalette();
             case HAIL_THUNDERSTORM -> ColorConstants.HAIL;
             default -> ColorConstants.DEFAULT_COLORS;
         };
@@ -84,6 +52,48 @@ public class ColorClimate {
         };
     }
 
+    private Color[] getSunnyPalette() {
+        if (temperature > 35) {
+            return ColorConstants.SUNNY_ABOVE_35;
+        } else if (temperature > 25) {
+            return ColorConstants.SUNNY_ABOVE_25_AND_BELOW_35;
+        } else {
+            return ColorConstants.SUNNY_BELOW_25;
+        }
+    }
+
+    private Color[] getSunnyCloudyPalette() {
+        if (temperature > 30) {
+            return ColorConstants.SUNNY_CLOUDY_ABOVE_30;
+        } else {
+            return ColorConstants.SUNNY_CLOUDY_BELOW_30;
+        }
+    }
+
+    private Color[] getCloudyPalette() {
+        if (temperature > 25) {
+            return ColorConstants.CLOUDY_ABOVE_25;
+        } else {
+            return ColorConstants.CLOUDY_BELOW_25;
+        }
+    }
+
+    private Color[] getRainPalette() {
+        if (temperature > 25) {
+            return ColorConstants.RAINY_ABOVE_25;
+        } else {
+            return ColorConstants.RAINY_BELOW_25;
+        }
+    }
+
+    private Color[] getThunderstormPalette() {
+        if (temperature > 25) {
+            return ColorConstants.THUNDERSTORM_ABOVE_25;
+        } else {
+            return ColorConstants.THUNDERSTORM_BELOW_25;
+        }
+    }
+
     public WeatherConditions getCondition() {
         return condition;
     }
@@ -92,5 +102,11 @@ public class ColorClimate {
         this.condition = condition;
     }
 
+    public double getTemperature() {
+        return temperature;
+    }
 
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
